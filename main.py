c72df91f8ff799970248d62af3662f2ffd0c7ef7
leand12 <leandrosilva12@ua.pt>
2022-02-06 09:35:39 +0000

    base argparse

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-from bloom_filter import BloomFilter
-
-
+import argparse
+from counter import ExactCounter, BloomFilterCounter
 
 
 if __name__ == '__main__':
-    ...
+
+    parser = argparse.ArgumentParser(
+        description='Distinct Word Counting Program')
+
+    parser.add_argument('file', type=argparse.FileType('r'),
+                        help='the path of the file with the words to be counted')
+    parser.add_argument('counter', choices=['exact', 'bloom'],
+                        help='the type of counter')
+
+    args = parser.parse_args()
+
+    exact = ExactCounter(args.file.name)
+    bloom = BloomFilterCounter(args.file.name)
+
+    exact.read_chars()
